203d079a5aab89a0632751a7aa9b430d9b774f43
xrr2016 <xiaoranran1993@163.com>
2017-04-05 23:25:52 +0800

    :art: learning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      todos: [
+        {id: 0, name: 'Learn React.', isComplete: false},
+        {id: 1, name: 'Learn JSX.', isComplete: false},
+        {id: 2, name: 'Learn Redux!', isComplete: false}
+      ]
+    }
+  }
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>React Todos</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='todo-app'>
+          <form>
+            <input type='text' />
+          </form>
+          <div className='todo-list'>
+            <ul>
+              {this.state.todos.map(todo =>
+                <li key={todo.id}><input type='checkbox' />{todo.name}</li>
+              )}
+            </ul>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

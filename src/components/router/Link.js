fe66382911c543679f6a956015b0a7c6e7399fea
xrr2016 <xiaoranran1993@163.com>
2017-04-08 23:19:03 +0800

    :bug: problem

--- src/components/router/Link.js
+++ src/components/router/Link.js
@@ -1,7 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 export class Link extends Component {
-  render () {
-    return <a href="#">{this.props.children}</a>
+  
+  static contextTypes = {
+    route: React.PropTypes.string,
+    linkHandler: React.PropTypes.func
   }
+
+  handleChick = (event) => {
+    event.preventDefault()
+    // history.pushState(null, '', this.props.to)
+    console.dir(this.context)
+    this.context.linkHandler(this.props.to)
+  }
+  
+  render () {
+    const activeClass = this.context.route === this.props.to ? 'active' : ''
+    return <a href="#" className={activeClass} onClick={this.handleChick}>{this.props.children}</a>
+  }
+}
+
+Link.propTypes = {
+  to: PropTypes.string.isRequired
 }
--- src/components/todo/Footer.js
+++ src/components/todo/Footer.js
@@ -3,10 +3,10 @@ import { Link } from '../router'
 
 export const Footer = () => {
   return (
-    <div>
-      <Link>All</Link>
-      <Link>Active</Link>
-      <Link>Complete</Link>
+    <div className='footer'>
+      <Link to='/'>All</Link>
+      <Link to='/active'>Active</Link>
+      <Link to='/complete'>Complete</Link>
     </div>
   )
 }
